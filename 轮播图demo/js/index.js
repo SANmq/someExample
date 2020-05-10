dae5a15d88ee51c6ecd7e8bc2debc99a17c19f90
SANmq <739699025@qq.com>
2020-05-10 23:25:10 +0800

    轮播图修改

--- 轮播图demo/js/index.js
+++ 轮播图demo/js/index.js
@@ -4,20 +4,64 @@ LoopView.prototype = {
         // 定义初始的展示位置大小,没有填充默认大小
         this.initData();
         this.render();
-        this.handle();
+        this.move(-1040)
     },
 
     initData: function () {
         //  初始化图片位置，
         this.index = 1;
+        this.timer1 = null;
+        this.timer2 = null;
+        //  计算有边界
+        this.rightborder = -this.width * (this.length + 1)
+        // 初始化每次移动步长
+        this.step = 0;
+        // 初始化下次的位置
+        this.nextDis = -this.width * this.index;
+        // 初始化当前的位置
+        this.nowDis = -this.width * this.index;
         // 初始化展布宽高
-        this.exhibition.style.width = this.width * (this.length + 2) + "px";
+        this.exhibition.style.width = -this.rightborder + this.width + "px";
         this.exhibition.style.height = this.height + "px";
         // 初始化展布位置
-        this.exhibition.style.left = -this.width * this.index + "px";
+        this.exhibition.style.left = this.nowDis + "px";
+        // 设置防抖
+        this.flag = true;
 
     },
 
+    handle() {
+
+    },
+    move(dis) {
+        this.nextDis = this.nowDis + dis;
+        this.step = parseInt(dis / 40) + 1;
+        self = this;
+        this.flag = false;
+        this.timer1 = setInterval(function () {
+            if (Math.abs(self.nextDis - self.nowDis) > Math.abs(self.nextDis - self.nowDis - self.step)) {
+                self.nowDis = self.nowDis + self.step;
+                self.exhibition.style.left = self.nowDis + 'px';
+            } else {
+                // 移动完成后取消定时器
+                clearInterval(self.timer1);
+                self.nowDis = self.nextDis;
+                // 超边界取消定时器并重置
+                if (self.nextDis == self.rightborder) {
+                    self.nowDis = -self.width;
+                }
+                if (self.nextDis == 0) {
+                    self.nowDis = -self.length * self.width;
+                }
+                self.exhibition.style.left = self.nowDis + 'px';
+                self.flag = true;
+            }
+        }, 10)
+
+
+    },
+
+    // 初始化渲染页面方法
     render: function () {
         // 产生临时插入值
         let contant1 = ""
@@ -38,117 +82,8 @@ LoopView.prototype = {
         this.controlBar.innerHTML = contant2;
         // 更新小圆点的位置
         this.button = this.controlBar.childNodes;
-        // this.button[this.index - 1].className = 'select';
-        this.bindpoint();
+        this.button[this.index - 1].className = 'select';
     },
-
-    // 更新小圆点的位置
-    bindpoint() {
-        for (let i = 0; i < this.length; i++) {
-            if (i == this.index - 1) {
-                this.button[i].className = 'select';
-            } else {
-                this.button[i].className = '';
-            }
-        }
-    },
-
-    move(dis) {
-        // 每次移动前先更新index的位置
-        // if (this.index - dis / this.width < 1) {
-        //     this.index = this.length;
-        // } else if (this.index - dis / this.width > this.length) {
-        //     this.index = 1;
-        // } else {
-        //     this.index -= dis / this.width;
-        // }
-        // this.bindpoint();
-
-        // 移动一次的动画时间
-        let time = 0.1;
-        // 移动一次的步长
-        let step = dis * 0.1;
-        let self = this;
-        let pos = parseInt(self.exhibition.style.left)
-
-        this.no1 = setInterval(function () {
-            self.exhibition.style.left += step
-        }, 10);
-
-
-
-
-
-
-        // let self = this;
-        // var allTime = 400;
-        // var eachTime = 10;
-        // var eachDis = dis / (allTime / eachTime);
-        // var newDis = parseInt(self.exhibition.style.left) + dis;
-        // // flag = false;
-        // // clearInterval(self.no1);
-        // function eachMove() {
-        //     if (dis < 0 && parseInt(self.exhibition.style.left) > newDis || dis > 0 && parseInt(self.exhibition.style.left) < newDis) {
-        //         self.exhibition.style.left = parseInt(self.exhibition.style.left) + eachDis + 'px';
-        //     } else {
-        //         flag = true;
-        //         clearInterval(self.no1);
-        //         self.exhibition.style.left = newDis + 'px';
-        //         //设置无限滚动
-        //         console.log(newDis)
-        //         if (newDis == 0) {
-        //             self.exhibition.style.left = -self.width * self.data.length + 'px';
-        //         }
-        //         if (newDis == -(self.width * (self.data.length + 1))) {
-        //             self.exhibition.style.left = -self.width + 'px';
-        //         }
-
-        //     }
-        // }
-        // self.no1 = setInterval(eachMove, 10);
-    },
-
-    handle: function () {
-        // this.handlemouseenter();
-        // this.handlemouseleave();
-        this.handleclick();
-    },
-
-    handlemouseleave() {
-        var self = this;
-        this.ele.onmouseleave = function (e) {
-            self.leftButton.style.display = 'none';
-            self.rightButton.style.display = 'none';
-        }
-    },
-
-    handlemouseenter() {
-        var self = this;
-        this.ele.onmouseenter = function (e) {
-            self.leftButton.style.display = 'block';
-            self.rightButton.style.display = 'block';
-        }
-    },
-
-    handleclick() {
-        var self = this;
-        this.leftButton.onclick = function () {
-
-        };
-
-        this.rightButton.onclick = function () {
-
-        };
-
-        for (let i = 0; i < this.length; i++) {
-            self.button[i].onclick = (function (j) {
-                return function () {
-                    self.index = j + 1;
-                    self.bindpoint();
-                }
-            }(i))
-        }
-    }
 }
 
 
@@ -167,4 +102,4 @@ function LoopView(ele, width, height, speed, data, leftButton, rightButton, cont
     this.init();
 }
 
-new LoopView();
+var l = new LoopView();
