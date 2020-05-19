26bfdf001272f36b9159e1c1b0125bbf307b7375
SANmq <739699025@qq.com>
2020-05-19 23:19:24 +0800

    更新
    能够实现弹性的来回运动

--- 运动.js
+++ 运动.js
@@ -40,14 +40,44 @@ function moveStart(dom, target) {
     }, 30)
 }
 
-// 弹性运动
-function moveSpring(dom, target) {
+// 弹性运动,物体根据轴心进行往复运动，最终停止在轴上
+function moveSpring(dom, axis, v0, k, f) {
+    // v0初始速度，矢量，同axis相同,k筋度系数
     clearInterval(dom.timer);
-
-    
-
+    let iSpeedX = v0.x,
+        iSpeedY = v0.y,
+        centerX = parseInt(getStyle(dom, 'width')) / 2,
+        centerY = parseInt(getStyle(dom, 'height')) / 2;
     dom.timer = setInterval(function () {
-
+        let icurX = centerX + parseInt(dom.offsetLeft),
+            icurY = centerY + parseInt(dom.offsetTop),
+            lx = axis.x - icurX,
+            ly = axis.y - icurY,
+            aX = lx * k,
+            aY = ly * k;
+        iSpeedX += aX;
+        iSpeedY += aY;
+        let nowX = icurX + iSpeedX,
+            nowY = icurY + iSpeedY;
+        if ((nowX - axis.x) * (icurX - axis.x) < 0) {
+            // 如果当前位置乘以之前的速度
+            iSpeedX *= f;
+            if (Math.abs(iSpeedX) < 1) {
+                iSpeedX = 0;
+                dom.style.left = axis.x - centerX + 'px';
+                flag = true
+            }
+        }
+        if ((nowY - axis.y) * (icurY - axis.y) < 0) {
+            // 如果当前位置乘以之前的速度
+            iSpeedY *= f;
+            if (Math.abs(iSpeedY) < 1) {
+                iSpeedY = 0;
+                dom.style.top = axis.y - centerY + 'px';
+            }
+        }
+        dom.style.left = nowX - centerX + 'px';
+        dom.style.top = nowY - centerY + 'px';
 
 
     }, 30)
@@ -55,11 +85,17 @@ function moveSpring(dom, target) {
 
 
 var btn = document.getElementById('btn');
-var target = {
-    left: 500,
-    width: 400,
-    opacity: 100,
+var axis = {
+    x: 400,
+    y: 50,
+    // z: 100,
 }
+
+var v0 = {
+    x: 0,
+    y: 0
+}
+
 btn.onclick = function () {
-    moveStart(this, target)
+    moveSpring(this, axis, v0, 0.1, 1)
 }
