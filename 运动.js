5234f24f80751017bc4d0107e83e3cc96b09ddd8
SANmq <739699025@qq.com>
2020-05-21 23:28:12 +0800

    优化物体运动
    部分计算完毕，继续优化物体运动

--- 运动.js
+++ 运动.js
@@ -84,31 +84,20 @@ function moveSpring(dom, axis, v0, k, f) {
 }
 
 
-var btn = document.getElementById('btn');
-var axis = {
-    x: 400,
-    y: 50,
-}
-
-var v0 = {
-    x: 60,
-    y: 0
-}
-
-btn.onclick = function () {
-    moveByG(this, v0, 3, true, 0.8)
-}
-
-function moveByG(dom, v0, g, rebound, eLost) {
-    // 物体仅受重力作用,rebound确定物体是否反弹,eLost反弹后的能量损
+function moveByG(dom, v0, g, rebound, eLost, boder) {
+    // 物体仅受重力作用,rebound确定物体是否反弹,eLost反弹后的能量损,boder 如果元素为非窗口定位元素，且设置此值，
+    // 则元素移动范围不会超出该元素所示区域
     clearInterval(dom.timer);
     let iSpeedX = v0.x,
-        iSpeedY = v0.y,
-        height = parseInt(getStyle(dom, "height")),
-        width = parseInt(getStyle(dom, "width"));
+        iSpeedY = v0.y;
+
     dom.timer = setInterval(function () {
-        let icurX = parseInt(dom.offsetLeft),
+
+        let height = dom.clientHeight,
+            width = dom.clientWidth,
+            icurX = parseInt(dom.offsetLeft),
             icurY = parseInt(dom.offsetTop);
+        console.log(height, width)
         iSpeedY += g;
         let nowX = icurX + iSpeedX,
             nowY = icurY + iSpeedY;
@@ -129,14 +118,42 @@ function moveByG(dom, v0, g, rebound, eLost) {
                     iSpeedY *= eLost;
                     iSpeedX *= eLost;
                 }
+                console.log()
             }
         }
-        // if (nowY + height == document.documentElement.clientHeight && iSpeedX) {
-
-        // }
-        dom.style.left = nowX + 'px';
-        dom.style.top = nowY + 'px';
-        console.log(iSpeedY)
-        console.log(iSpeedX)
+        if (nowY + height == document.documentElement.clientHeight && Math.abs(iSpeedX) <= 1 && Math.abs(iSpeedY) <= 1 + g / (eLost + 1)) {
+            // console.log(iSpeedY)
+            clearInterval(dom.timer)
+        } else {
+            dom.style.left = nowX + 'px';
+            dom.style.top = nowY + 'px';
+        }
     }, 30)
+}
+
+
+var btn = document.getElementById('btn');
+var axis = {
+    x: 400,
+    y: 50,
+}
+
+var v0 = {
+    x: 60,
+    y: 0
+}
+
+btn.onclick = function () {
+    move(this, v0, 3, true, 0.8)
+}
+
+function move(ele, boder, v0, a, lost, isKnock) {
+    // ele运动元素，
+    // boder 运动区域（填元素，默认元素padding区域），如果元素相对视口定位，则添加无效
+    // v0 运动初速度 a运动加速度
+    // lost 碰撞后的速度损失
+    // isKnock 是否发生碰撞行为，如果boder设置或元素定位相对视口,则默认发生碰撞行为
+
+
+
 }
